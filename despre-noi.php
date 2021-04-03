a7d82b0a7f6f7d770aa68a459f0c1ffe2664af97
nemo-aristos <81801223+nemo-aristos@users.noreply.github.com>
2021-04-03 16:46:59 +0300

    Add files via upload

---
+++ despre-noi.php
@@ -0,0 +1,80 @@
+<?php include 'C:\Users\admin\Desktop\Proiect site\includes\header.php';?>
+
+
+
+<div class="container">
+        <h1>Despre noi</h1>
+
+        <p>
+         Lorem ipsum dolor sit amet, sonet inimicus moderatius at vel, per in esse falli paulo. Est perfecto tacimates an, eos assum augue cu, porro antiopam vituperata has cu. Eum ei mollis facilis.Illud atqui in mei. Porro facer veritus cu quo, no pri graeci volutpat contentiones, ne per rebum antiopam dignissim.
+        </p>
+
+        <p>
+         Natum sonet regione cum id, vitae tamquam mei ei, sit aliquip delicata splendide ne. Illud atqui in mei. Porro facer veritus cu quo, no pri graeci volutpat contentiones, ne per rebum antiopam dignissim.
+        </p>
+
+        <p>
+         <a class="btn btn-outline-primary" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+              Citeste mai mult
+         </a>
+        </p>
+
+
+
+
+          <div class="collapse" id="collapseExample">
+            <div class="card card-body">
+                Sonet regione cum id, vitae tamquam mei ei, sit aliquip delicata splendide ne. Illud atqui in mei. Porro facer veritus cu quo, no pri graeci volutpat contentiones, ne per rebum antiopam dignissim.Natum sonet regione cum id, vitae tamquam mei ei, sit aliquip delicata splendide ne. Illud atqui in mei. Porro facer veritus cu quo, no pri graeci volutpat contentiones, ne per rebum antiopam dignissim.
+                <br>
+                <br>   
+                Lorem ipsum dolor sit amet, sonet inimicus moderatius at vel, per in esse falli paulo. Est perfecto tacimates an, eos assum augue cu, porro antiopam vituperata has cu. Eum ei mollis facilis.
+
+                
+                Natum sonet regione cum id, vitae tamquam mei ei, sit aliquip delicata splendide ne. Illud atqui in mei. Porro facer veritus cu quo, no pri graeci volutpat contentiones, ne per rebum antiopam dignissim.
+                
+            </div>
+          </div>
+
+
+             <h2>Echipa</h2>
+
+
+            <div class="row d-flex">
+
+                <div class="col-4">
+
+                    <div class="card">
+                        <img src="https://images.unsplash.com/photo-1569007889977-42e2392850d7?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTZ8fHBlcnNvbnxlbnwwfDJ8MHxibGFja19hbmRfd2hpdGU%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="...">
+                        <div class="card-body">
+                          <p class="card-text"><strong>Florica Petrescu </strong> <br><strong> PR</strong></p>
+                        </div>
+                </div>
+
+                 </div>   
+
+                <div class="col-4">
+                    <div class="card">
+                        <img src="https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fHBlcnNvbnxlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text"><strong>Gica Petrescu</strong>  <br> <strong>CEO</strong></p>
+                        </div>
+                     </div>
+                </div>
+
+                <div class="col-4">
+
+                    <div class="card">
+                        <img src="https://images.unsplash.com/photo-1551712702-4b7335dd8706?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjR8fHBlcnNvbnxlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text"><strong>Geanina Petrescu </strong> <br><strong> Manager</strong></p>
+                    </div>
+                </div>
+
+            </div>    </div>
+</div>         
+
+
+
+
+        
+<?php include 'C:\Users\admin\Desktop\Proiect site\includes\footer.php';?>
---
+++ includes/footer.php
@@ -0,0 +1,27 @@
+<div class="flower-footer text-center pt-5 pb-5">
+        <div class="flower">
+            <img src="pictures\spring.png" alt="">
+        </div>
+                  
+                <form action="label" class="form-inline">Aboneaza-te la newsletter!</form>
+
+                <div class="row bs-gx-0 me-auto">
+                    <div class="col-4 mx-auto pb-2" >
+                       <div class="input-group">
+                       <div class="input input-outline-secondary "></div>
+                       <input type="text" class="form-control" placeholder="Adresa de email">
+                       <button type="button" class="btn btn-primary text-center mx-auto">Trimite!</button>
+                       </div>  
+                    </div>
+             
+                <p>&#169; Toate drepturile rezervate! <?php $year = date("2021"); echo $year; ?> </p>
+                
+                 </div>
+    </div> 
+    
+
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+               
+    </body>
+
+</html>
